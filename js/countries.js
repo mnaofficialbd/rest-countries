d63e5b394d54482572a36ef5cb1bce44ea7566fb
mnaofficial <mna.official92@gmail.com>
2022-03-14 22:40:23 +0600

    final commit

--- js/countries.js
+++ js/countries.js
@@ -8,8 +8,8 @@ const loadCountries = () => {
 const displayCountries = (countries) => {
     const countriesHTML = countries.map(country => getCountryHTML(country));
 
-    // console.log(countries)
-    console.log(countriesHTML[0]);
+    console.log(countries[0])
+    // console.log(countriesHTML[0]);
 
     const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
@@ -18,8 +18,10 @@ const displayCountries = (countries) => {
 const getCountryHTML = country => {
     return `
     <div class="country">
-       <h2>${country.name.common}</h2>
-       <img src="${country.flags.png}">
+    <h2>${country.name.common}</h2>
+    <img src="${country.flags.png}">
+    <h3>Capital: ${country.capital}</h3>
+    <h3>Population: ${country.population}</h3>
     </div>`
 }
 
