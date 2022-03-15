4295f6f09d205ad14b90338b3b24a2835110bda9
mnaofficial <mna.official92@gmail.com>
2022-03-15 13:23:17 +0600

    Done

--- js/countries.js
+++ js/countries.js
@@ -8,13 +8,14 @@ const loadCountries = () => {
 const displayCountries = (countries) => {
     const countriesHTML = countries.map(country => getCountryHTML(country));
 
-    console.log(countries[0])
+    console.log(countries[1])
     // console.log(countriesHTML[0]);
 
     const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
 }
 
+/* original=====
 const getCountryHTML = country => {
     return `
     <div class="country">
@@ -22,6 +23,38 @@ const getCountryHTML = country => {
     <img src="${country.flags.png}">
     <h3>Capital: ${country.capital}</h3>
     <h3>Population: ${country.population}</h3>
+    <h3>Region: ${region}</h3>
+    </div>`
+} 
+*/
+
+/* option-1======
+const getCountryHTML = country => {
+    const { name, flags, capital, population } = country;
+    return `
+    <div class="country">
+    <h2>${name.common}</h2>
+    <img src="${flags.png}">
+    <h3>Capital: ${capital}</h3>
+    <h3>Population: ${population}</h3>
+    <h3>Region: ${region}</h3>
+    </div>`
+} 
+*/
+
+//option-2======
+const getCountryHTML = ({ name, flags, capital, population, region, area, status, subregion }) => {
+    return `
+    <div class="country">
+    <h2>${name.common}</h2>
+    <img src="${flags.png}">
+    <h3>Official Name: ${name.official}</h3>
+    <h3>Capital: ${capital}</h3>
+    <h3>Population: ${population}</h3>
+    <h3>Region: ${region}</h3>
+    <h3>subregion: ${subregion}</h3>
+    <h3>status: ${status}</h3>
+    <h3>Area: ${area}</h3>
     </div>`
 }
 
